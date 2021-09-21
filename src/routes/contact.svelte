8a5c2c1675d30b030cb21c2ee9e7a2574fa10003
garrettbland <gmorganbland@gmail.com>
2021-09-20 23:29:13 -0500

    please work

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -17,10 +17,9 @@
         method="POST"
         action="/success"
         data-netlify="true"
-        netlify-honeypot="company"
     >
         <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="company" hidden />
+
         <div class="grid grid-cols-1 md:grid-cols-2 gap-5 mb-4">
             <input
                 class=""
